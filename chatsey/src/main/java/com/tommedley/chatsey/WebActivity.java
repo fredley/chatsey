4f83c5470c2df3b341ab0383b33422d0561be5b0
fredley <fredley@gmail.com>
2014-04-18 17:45:08 +0000

    Initial commit

---
+++ chatsey/src/main/java/com/tommedley/chatsey/WebActivity.java
@@ -0,0 +1,93 @@
+package com.tommedley.chatsey;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebActivity extends Activity {
+
+    private static final String USER_AGENT_STRING = "Chatsey";
+    private static final String TAG = "WebActivity";
+    private WebView mWebView;
+
+    private class ChatWebViewClient extends WebViewClient {
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl){
+            Log.d(TAG, "Error loading page: " + description);
+            view.stopLoading();
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            view.loadUrl("javascript:(function() {" +
+                    "var parent = document.getElementsByTagName('head').item(0);" +
+                    "var script = document.createElement('script');" +
+                    "script.type = 'text/javascript';" +
+                    "script.src = 'http://tommedley.com/files/chatsey.js';" +
+                    "parent.appendChild(script);" +
+                    "alert('Trying to inject');" +
+                    "})()");
+            Log.d(TAG,"Injected");
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (Uri.parse(url).getHost().equals("chat.stackexchange.com") ||
+                    Uri.parse(url).getHost().equals("chat.stackoverflow.com") ||
+                    Uri.parse(url).getHost().equals("stackexchange.com") ||
+                    Uri.parse(url).getHost().endsWith("google.com") ||
+                    Uri.parse(url).getHost().endsWith("google.co.uk")) {
+                return false;
+            }else{
+                Log.i(TAG,"Allowed URL: " + url);
+            }
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_web);
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.setWebViewClient(new ChatWebViewClient());
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            mWebView.setWebContentsDebuggingEnabled(true);
+        }
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setUserAgentString(USER_AGENT_STRING);
+        mWebView.loadUrl("http://chat.stackexchange.com/");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mWebView.reload();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
